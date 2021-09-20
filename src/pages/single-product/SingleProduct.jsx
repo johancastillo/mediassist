17fdb4d384b446665866886f7a27113057f5f4d0
Johan Castillo <jcjphan2707@gmail.com>
2021-09-19 20:45:20 -0400

    Glucome product

--- src/pages/single-product/SingleProduct.jsx
+++ src/pages/single-product/SingleProduct.jsx
@@ -8,11 +8,71 @@ const SingleProduct = () => {
             <div className="row">
                 
                 <div className="col-md-6">
-                    <img src="" alt="" />
+                    <img src="./assets/images/products/04.jpeg" width="100%" alt="" />
                 </div>
                 
                 <div className="col-md-6">
-                    <h1>Single Product</h1>
+                    <h1>Glucome</h1>
+
+                    <p>
+                    Es un dispositivo digital único en el mercado, que simplifica el control de su diabetes y reduce la carga del monitoreo de la diabetes. El monitor de glucosa en sangre GlucoMe mide sus niveles de glucosa y los transmite sin inconvenientes a cualquier dispositivo móvil, ya sea iOS o Android.  
+                    </p>
+
+                    <p>
+                    Una fuente confiable de información de salud actualizada que está disponible inmediatamente para pacientes y tutores por medio de la App , también disponible para los médicos o enfermeras por medio de la Clínica Digital de Diabetes en la nube GlucoMe. GlucoMe disminuye los sobrecostos de cuidados, ayuda a los pacientes a cumplir con sus planes de tratamiento, mejora sus niveles de HbA1c e impacta sobre la calidad de vida general del paciente. Entre sus características:
+                    </p>
+
+                    <ul>
+                        <li>
+                        Conexión inalámbrica a todos los teléfonos móviles apagado.
+                        </li>
+
+                        <li>
+                            Medición de glucosa.
+                        </li>
+
+                        <li>
+                        Uso de insulina .
+                        </li>
+
+                        <li>
+                        Conectividad de audio única.
+                        </li>
+
+                        <li>
+                        Sin pantalla.
+                        </li>
+
+                        <li>
+                        NO teclas.
+                        </li>
+
+                        <li>
+                        NO botón de encendido.
+                        </li>
+
+                        <li>
+                        NO Bluetooth.
+                        </li>
+
+                        <li>
+                        NO conector.
+                        </li>
+
+                        <li>
+                        NO RFID / NFC.
+                        </li>
+
+                        <li>
+                        NO wifi.
+                        </li>
+
+                        <li>
+                        NO conectividad celular.
+                        </li>
+
+                    </ul>
+
                 </div>
 
             </div>
